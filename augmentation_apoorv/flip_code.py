cf73fa9235681499427350dd658d4cd5972d3f84
Apoorv012 <apoorvmitthu@gmail.com>
2022-08-17 22:29:44 +0530

    Made the code to augment the data via flipping

--- augmentation_apoorv/flip_code.py
+++ augmentation_apoorv/flip_code.py
@@ -21,10 +21,30 @@ for category in categories:
         img_name = img.split(".")[0]
         # print(img_name)
 
-        curr_path = os.path.join(img_path, category, img_name + ".png")
-        final_path = os.path.join(img_path, category, img_name + "_flipped.png")
+        # Initialization
+        curr_path_img = os.path.join(img_path, category, img_name + ".png")
+        final_path_img = os.path.join(img_path, category, img_name + "_flipped.png")
 
-        image = Image.open(curr_path)
-        image_flipped = image.transpose(Image.FLIP_LEFT_RIGHT)
-        image_flipped.save(final_path, "png")
+        # Image Manupulation
+        image = Image.open(curr_path_img)
+        image_flipped = image.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
+        image_flipped.save(final_path_img, "png")
+
+        curr_path_label = os.path.join(label_path, category, img_name + ".txt")
+        final_path_label = os.path.join(label_path, category, img_name + "_flipped.txt")
+
+
+        # Label Manupulation
+        with open(curr_path_label, "r") as fh:
+            txt_data = fh.read()
+
+        txt_data_lst = [float(x) for x in txt_data.split()]
+        txt_data_lst[1] = 1 - txt_data_lst[1]
+        txt_data_lst[2] = 1 - txt_data_lst[2]
+        txt_data = " ".join(str(x) for x in txt_data_lst)
+
+        with open(final_path_label, "w") as fh:
+            fh.write(txt_data)
+        
+        # For the time being
         break
